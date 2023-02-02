7f543c999cec88c3da743ed73648932b8b0b7aaf
Jan Kaifer <jan@kaifer.cz>
2023-02-02 13:37:39 +0100

    added repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import dynamic from "next/dynamic";
+
 /** Add your relevant code here for the issue to reproduce */
+const Comp = dynamic(() => import("#/app/file2").then((mod) => mod.default));
+
 export default function Home() {
-  return null
+  return <Comp />;
 }
